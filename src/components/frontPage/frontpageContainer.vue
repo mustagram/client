8fdccd4552b81c9ceb9635052eab24af4b937e7b
michaelwp <michael.wenceslaus@gmail.com>
2020-01-17 08:01:36 +0700

    fix login register design

---
+++ src/components/frontPage/frontpageContainer.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="backgroundContainer">
+        <loginLogo></loginLogo>
+<!--        <div class="main-content">-->
+            <Register v-on:click="setVisible" v-if="!visible"></Register>
+            <Login v-on:register="setVisible" v-if="visible"></Login>
+<!--        </div>-->
+    </div>
+</template>
+
+<script>
+    import Register from "./register"
+    import Login from "./login"
+    import loginLogo from "./loginLogo"
+
+    export default {
+        name: "App",
+        data() {
+            return {
+                visible: true,
+            }
+        },
+        methods: {
+            setVisible() {
+                if (!this.visible) {
+                    this.visible = true
+                } else {
+                    this.visible = false
+                }
+            }
+        },
+        components: {
+            Register,
+            Login,
+            loginLogo
+        }
+    }
+</script>
+
+<style scoped>
+    .backgroundContainer {
+        text-align: center;
+        position: relative;
+        /* z-index: -1; */
+        height: 100vh;
+        background-image: linear-gradient(
+                to right bottom,
+                #c8cec7c7,
+                #949796c0),
+        url(../../img/fron-image.jpg);
+        background-size: cover;
+        background-position: top;
+    }
+
+    .main-content {
+        /*position: absolute;*/
+        /*!* z-index: 1; *!*/
+        /*!* top: 50%; *!*/
+        /*background-color: #ffff;*/
+        /*left: 50%;*/
+        /*transform: translate(-50%, 50%);*/
+        /*border-style: solid;*/
+        /*border-width: 3px;*/
+        /*border-radius: 15px;*/
+        /*border-color: #b2bec3a9;*/
+        /*box-shadow: 0px 10px 10px #2d3436c2;*/
+    }
+</style>
---
+++ src/components/frontPage/loginLogo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="logo-app">
+        <h1 class="font-logo-app">MustaGram</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "loginLogo"
+    }
+</script>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css?family=Righteous&display=swap');
+
+    .font-logo-app {
+        font-size: 100px;
+        color: #ecf1f3;
+        font-family: 'Righteous', cursive;
+    }
+
+    .logo-app {
+        /*position: absolute;*/
+        /*text-align: center;*/
+        /*left: 50%;*/
+        /*transform: translate(-50%, 50%);*/
+        margin: 20px;
+    }
+</style>
